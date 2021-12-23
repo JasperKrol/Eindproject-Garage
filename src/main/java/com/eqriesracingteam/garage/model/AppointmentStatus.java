81b47e6b295691563e6792837f17f3d643b85860
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-12-23 16:00:10 +0100

    added appointmentstatus and start testscripts

---
+++ src/main/java/com/eqriesracingteam/garage/model/AppointmentStatus.java
@@ -0,0 +1,5 @@
+package com.eqriesracingteam.garage.model;
+
+public enum AppointmentStatus {
+    AFSPRAAK_GEPLAND, AKKOORD_KLANT, NIET_UITVOEREN, REPARATIE_UITGEVOERD, BETAALD
+    }
---
+++ src/test/java/com/eqriesracingteam/garage/controller/AuthenticationControllerTest.java
@@ -0,0 +1,17 @@
+package com.eqriesracingteam.garage.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createAuthenticationToken() {
+    }
+}
