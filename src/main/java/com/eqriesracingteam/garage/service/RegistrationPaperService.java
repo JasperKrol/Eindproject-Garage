e9b6c8c811467dc4995f7c48ec8797ba9aa60ac2
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2022-01-17 12:13:02 +0100

    removed some unused imports

--- src/main/java/com/eqriesracingteam/garage/service/RegistrationPaperService.java
+++ src/main/java/com/eqriesracingteam/garage/service/RegistrationPaperService.java
@@ -4,17 +4,11 @@ import com.eqriesracingteam.garage.exceptions.RecordNotFoundException;
 import com.eqriesracingteam.garage.model.RegistrationPaper;
 import com.eqriesracingteam.garage.repository.RegistrationPaperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 @Service
